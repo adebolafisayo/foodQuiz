1591bb9f1cd9f6d46de5a30d48e6c6f2f07b2307
adebolafisayo Fisayo <debbyfissy@yahoo.com>
2024-04-14 17:08:28 +0000

    displayRandomFunction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-function foodName(){
- 
-}
+const foods = ['Apple', 'Banana', 'Orange', 'Carrot', 'Broccoli', 'Chicken', 'Fish', 'Pasta', 'Pizza', 'Salad'];
 
-
-    function displayRandomFood() {
-        let foods =[rice,pineapple,egg,water,butter,zinc];
-        const randomIndex = Math.floor(Math.random() * foods.length);
-        const randomFood = foods[randomIndex];
-        const randomFoodField = document.getElementById('randomFood');
-        randomFoodField.textContent = randomFood;
+function displayRandomFood() {
+    const randomIndex = Math.floor(Math.random() * foods.length);
+    const randomFood = foods[randomIndex];
+    let buttonFields = document.getElementsByTagName('button');
+    for (let i = 0; i < buttonFields.length; i++) {
+        buttonFields[i].addEventListener('click', function() {
+            alert(`${i}`);
+        });
+    }
+    const randomFoodField = document.getElementById('randomFood');
+    
+    randomFoodField.textContent = randomFood;
 }
